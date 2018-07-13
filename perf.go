25510f5d21f649de0ba239f02b3be28dc30ef411
Kirk Scheibelhut <kjs@google.com>
2018-07-13 10:15:23 -0700

    Expose CpM/CpF

--- perf.go
+++ perf.go
@@ -18,18 +18,38 @@ const cdaF = 0.300 // 1.67m
 // of distance d in metres, gradient gr (rise/run) and median elevation h in
 // metres for a male rider.
 func CalcM(t, d, gr, h float64) float64 {
-	return score(t, twr(d, gr, h, mrM, cdaM, cpM))
+	return score(t, twr(d, gr, h, mrM, cdaM, CpM))
 }
 
 // CalcF calculates the PERF score for a performance of duration t on a climb
 // of distance d in metres, gradient gr (rise/run) and median elevation h in
 // metres for a female rider.
 func CalcF(t, d, gr, h float64) float64 {
-	return score(t, twr(d, gr, h, mrF, cdaF, cpF))
+	return score(t, twr(d, gr, h, mrF, cdaF, CpF))
 }
 
-func score(t, wr float64) float64 {
-	return 1000 * math.Pow(wr/t, 2)
+// CpM returns the expected power maintable for a male rider during a world
+// record level performance of duration t.
+//
+// MALE (67 kg)
+// 1 min: 11.5 W/kg
+// 5min:  7.60 W/kg
+// 60min: 6.40 W/kg
+// 770 W/509 W/429 W
+func CpM(t float64) float64 {
+	return 422.58 + 23296.7801287949/t
+}
+
+// CpF returns the expected power maintable for a female rider during a world
+// record level performance of duration t.
+//
+// FEMALE (53 kg)
+// 1 min: 9.29 W/kg
+// 5min:  6.61 W/kg
+// 60min: 5.69 W/kg
+// 492 W/350 W/302 W
+func CpF(t float64) float64 {
+	return 298.29 + 13499.9080036799/t
 }
 
 func twr(d, gr, h, mr, cda float64, cp func(float64) float64) float64 {
@@ -61,20 +81,6 @@ func twr(d, gr, h, mr, cda float64, cp func(float64) float64) float64 {
 	return tm
 }
 
-// MALE (67 kg)
-// 1 min: 11.5 W/kg
-// 5min:  7.60 W/kg
-// 60min: 6.40 W/kg
-// 770 W/509 W/429 W
-func cpM(t float64) float64 {
-	return 422.58 + 23296.7801287949/t
-}
-
-// FEMALE (53 kg)
-// 1 min: 9.29 W/kg
-// 5min:  6.61 W/kg
-// 60min: 5.69 W/kg
-// 492 W/350 W/302 W
-func cpF(t float64) float64 {
-	return 298.29 + 13499.9080036799/t
+func score(t, wr float64) float64 {
+	return 1000 * math.Pow(wr/t, 2)
 }
